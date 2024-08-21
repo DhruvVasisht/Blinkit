58bd39d19eb515e88b034b4b08516b2c1c2f31a4
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-08-21 23:31:46 +0530

    Session

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const indexRouter = require('./routes');
 const authRouter= require('./routes/auth');
 const path = require('path');
 const connectDB=require("./config/mongoose-connection");
+const expressSession = require('express-session');
 require('dotenv').config();
 require("./config/google_oauth_config");
 
 app.set('view engine', 'ejs');
 connectDB();
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(expressSession({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.SESSION_SECRET,
+}));
 
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
